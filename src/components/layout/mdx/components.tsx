ea472690f42ce12c74b4853e00842f5d4ad2ba2d
tt-lab-general <100188261+tt-lab-general@users.noreply.github.com>
2024-03-22 10:14:20 +0900

    Add mdx components

--- src/components/layout/mdx/components.tsx
+++ src/components/layout/mdx/components.tsx
@@ -1,10 +1,18 @@
 import { MDXComponents } from "mdx/types";
 import { Layout } from "./Layout";
 import { Ja, En } from "../../language";
+import { Typography } from "../../common";
 
 // TODO: Add components
 export const mdxComponents: MDXComponents = {
   wrapper: (props) => <Layout {...props} />,
   Ja: Ja,
   En: En,
+  h1: ({ ref, ...props }) => <Typography {...props} variant="h1" />,
+  h2: ({ ref, ...props }) => <Typography {...props} variant="h2" />,
+  h3: ({ ref, ...props }) => <Typography {...props} variant="h3" />,
+  h4: ({ ref, ...props }) => <Typography {...props} variant="h4" />,
+  h5: ({ ref, ...props }) => <Typography {...props} variant="h5" />,
+  h6: ({ ref, ...props }) => <Typography {...props} variant="h6" />,
+  p: ({ ref, ...props }) => <Typography {...props} variant="body1" />,
 };
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,14 +1,23 @@
 import { createTheme, responsiveFontSizes } from "@mui/material";
 import "@fontsource/noto-sans-jp";
 import "@fontsource/noto-serif-jp";
-import { red } from "@mui/material/colors";
+import { red, grey } from "@mui/material/colors";
+
+const bodyFontFamily = ["Noto Serif JP", "sans-serif"].join(",");
+const headingFontFamily = ["Noto Serif JP", "serif"].join(",");
 
 const _theme = createTheme({
   palette: {
     primary: red,
   },
   typography: {
-    fontFamily: ["Noto Serif JP", "sans-serif"].join(","),
+    fontFamily: bodyFontFamily,
+    h1: {
+      fontFamily: headingFontFamily,
+    },
+    h2: {
+      fontFamily: headingFontFamily,
+    },
   },
   components: {
     MuiAppBar: {
