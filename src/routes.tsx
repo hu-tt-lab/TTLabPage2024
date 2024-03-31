9357f8bb9ff1b792d25a761bdf9ec199bfb80a4d
ShijiMi-Soup <takahiro.zt899@gmail.com>
2024-03-31 21:41:05 +0900

    Add helmet

--- src/routes.tsx
+++ src/routes.tsx
@@ -11,6 +11,7 @@ import {
   Typography,
 } from "./components/common";
 import { ReactNode } from "react";
+import { Helmet } from "react-helmet";
 
 // ページ
 import Home from "./pages/home.mdx";
@@ -26,11 +27,13 @@ import Studylife from "./pages/studylife.mdx";
 import Books from "./pages/books.mdx";
 import Internship from "./pages/internship.mdx";
 import Outreach from "./pages/outreach.mdx";
+import { MDXProps } from "mdx/types";
+import { LangType, useLangContext } from "./components/language";
 
 type _Route = {
   path: string;
-  navLabel: string;
-  element: ReactNode;
+  navLabel: { [L in LangType]: string };
+  element: (props: MDXProps) => JSX.Element;
   children?: _Route[];
 };
 
@@ -38,77 +41,94 @@ type _Route = {
 const ROUTES: { [key: string]: _Route } = {
   home: {
     path: "/",
-    navLabel: "Home",
-    element: <Home />,
+    navLabel: { en: "Home", ja: "ホーム" },
+    element: Home,
   },
   about: {
     path: "/about",
-    navLabel: "About",
-    element: <About />,
+    navLabel: { en: "About", ja: "研究室について" },
+    element: About,
   },
   members: {
     path: "/members",
-    navLabel: "Members",
-    element: <Members />,
+    navLabel: { en: "Members", ja: "構成員" },
+    element: Members,
   },
   gallery: {
     path: "/gallery",
-    navLabel: "Gallery",
-    element: <Gallery />,
+    navLabel: { en: "Gallery", ja: "ギャラリー" },
+    element: Gallery,
   },
   projects: {
     path: "/projects",
-    navLabel: "Projects",
-    element: <Projects />,
+    navLabel: { en: "Projects", ja: "研究プロジェクト" },
+    element: Projects,
   },
   publications: {
     path: "/publications",
-    navLabel: "Publications",
-    element: <Publications />,
+    navLabel: { en: "Publications", ja: "研究業績" },
+    element: Publications,
   },
   awards: {
     path: "/awards",
-    navLabel: "Awards",
-    element: <Awards />,
+    navLabel: { en: "Awards", ja: "受賞" },
+    element: Awards,
   },
   theme: {
     path: "/theme",
-    navLabel: "Themes",
-    element: <Theme />,
+    navLabel: { en: "Themes", ja: "研究テーマ" },
+    element: Theme,
   },
   highlights: {
     path: "/highlight",
-    navLabel: "Highlights",
-    element: <Highlights />,
+    navLabel: { en: "Highlights", ja: "研究ハイライト" },
+    element: Highlights,
   },
   studylife: {
     path: "/studylife",
-    navLabel: "Life in our lab",
-    element: <Studylife />,
+    navLabel: { en: "Life in our lab", ja: "研究室での生活" },
+    element: Studylife,
   },
   books: {
     path: "/books",
-    navLabel: "Books",
-    element: <Books />,
+    navLabel: { en: "Books", ja: "参考図書" },
+    element: Books,
   },
   internship: {
     path: "/internship",
-    navLabel: "Internship",
-    element: <Internship />,
+    navLabel: { en: "Internship", ja: "インターンシップ" },
+    element: Internship,
   },
   outreach: {
     path: "/outreach",
-    navLabel: "Outreach",
-    element: <Outreach />,
+    navLabel: { en: "Outreach", ja: "アウトリーチ活動" },
+    element: Outreach,
   },
 };
 
 // ルーティング
 export function Routes() {
+  const lang = useLangContext()[0];
+
   return (
     <RRDRoutes>
       {Object.values(ROUTES).map((route) => (
-        <Route key={route.path} path={route.path} element={route.element} />
+        <Route
+          key={route.path}
+          path={route.path}
+          element={
+            <>
+              <Helmet>
+                <title>
+                  {lang == "ja"
+                    ? `${route.navLabel.ja} | 神経制御工学研究室`
+                    : `${route.navLabel.en} | Neural Control Engineering Lab`}
+                </title>
+              </Helmet>
+              <route.element />
+            </>
+          }
+        />
       ))}
     </RRDRoutes>
   );
@@ -119,11 +139,12 @@ export type RouteListProps = {
   onClick?: () => void;
 };
 export function RouteList({ onClick }: RouteListProps) {
+  const lang = useLangContext()[0];
   return (
     <List>
       {Object.values(ROUTES).map((route) => (
         <RouteListItem key={route.path} onClick={onClick} to={route.path}>
-          {route.navLabel}
+          {route.navLabel[lang]}
         </RouteListItem>
       ))}
     </List>
